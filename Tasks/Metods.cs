f20ed1599cd36d5378a060c0a79242b7c855b253
Максим Ночевный <bagor6268@gmail.com>
2023-01-13 19:32:49 +0300

    Добавьте файлы проекта.

---
+++ Tasks/Metods.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Tasks
+{
+    class Metods
+    {
+        public void Task1(int oneNumber, int twoNumber)
+        {
+            if (oneNumber == twoNumber)
+            {
+                Console.WriteLine("Два числа равны");
+            }
+            if (oneNumber > twoNumber)
+            {
+                Console.WriteLine("Первое число больше второго");
+            }
+            if (oneNumber < twoNumber)
+            {
+                Console.WriteLine("Первое число меньше второго");
+            }
+        }
+
+        public void Task2(int number)
+        {
+            if (number > 5 && number < 10)
+            {
+                Console.WriteLine("Число больше 5 и меньше 10");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число");
+            }
+        }
+
+        public void Task3(int number)
+        {
+            if (number == 5 || number == 10)
+            {
+                Console.WriteLine("Число либо равно 5, либо равно 10");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число");
+            }
+        }
+
+        public void Task4(double number, double year)
+        {
+            if (year >= 3)
+            {
+                if (number < 100)
+                {
+                    number += number * 0.07 * year;
+                    Console.WriteLine("Сумма с начисленным процентами: " + number);
+                }
+                else if (number < 200)
+                {
+                    number += number * 0.09 * year;
+                    Console.WriteLine("Сумма с начисленным процентами: " + number);
+                }
+                else if (number > 200)
+                {
+                    number += number * 0.12 * year;
+                    Console.WriteLine("Сумма с начисленным процентами: " + number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вклад выдается от 3-х лет");
+            }
+
+        }
+
+
+        public void Task5(double number, double year, double bonus)
+        {
+            double result;
+
+            if (year >= 3)
+            {
+                if (number < 100)
+                {
+                    number += number * 0.07 * year;
+                    result = number + bonus;
+                    Console.WriteLine("Сумма с начисленным процентами: " + number);
+                    Console.WriteLine("Финальная сумма с начисленным процентами и бонусом от банка: " + result);
+                }
+                else if (number < 200)
+                {
+                    number += number * 0.09 * year;
+                    result = number + bonus;
+                    Console.WriteLine("Сумма с начисленным процентами: " + number);
+                    Console.WriteLine("Финальная сумма с начисленным процентами и бонусом от банка: " + result);
+                }
+                else
+                {
+                    number += number * 0.12 * year;
+                    result = number + bonus;
+                    Console.WriteLine("Сумма с начисленным процентами: " + number);
+                    Console.WriteLine("Финальная сумма с начисленным процентами и бонусом от банка: " + result);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вклад выдается от 3-х лет");
+            }
+        }
+
+    }
+}
---
+++ Tasks/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+
+            Metods metods = new Metods();
+          
+            do
+            {
+                Console.Clear();
+               
+
+                Console.WriteLine("Введите номер задания: ");
+                int numberTask = Convert.ToInt32(Console.ReadLine());
+                switch (numberTask)
+                {
+                    case 1:
+                        Console.WriteLine("\nЗадание 1: Напишите консольную программу, в которую пользователь вводит с клавиатуры два числа. " +
+                            "А программа сравнивает два введенных числа и выводит на консоль результат сравнения " +
+                            "(два числа равны, первое число больше второго или первое число меньше второго).\n");
+
+                        Console.WriteLine("Введите первое число: ");
+                        int oneNumberT1 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Введите второе число: ");
+                        int twoNumberT1 = Convert.ToInt32(Console.ReadLine());
+
+                        metods.Task1(oneNumberT1, twoNumberT1);
+                        break;
+                    case 2:
+                        Console.WriteLine("\nЗадание 2: Напишите консольную программу, в которую пользователь вводит с клавиатуры число. " +
+                            "Если число одновременно больше 5 и меньше 10, то программа выводит \"Число больше 5 и меньше 10\". " +
+                            "Иначе программа выводит сообщение \"Неизвестное число\". \n");
+
+                        Console.WriteLine("Введите число");
+                        int numberT2 = Convert.ToInt32(Console.ReadLine());
+
+                        metods.Task2(numberT2);
+                        break;
+                    case 3:
+                        Console.WriteLine("\nЗадание 3: Напишите консольную программу, в которую пользователь вводит с клавиатуры число. " +
+                            "Если число либо равно 5, либо равно 10, то программа выводит \"Число либо равно 5, либо равно 10\". " +
+                            "Иначе программа выводит сообщение \"Неизвестное число\".\n");
+
+                        Console.WriteLine("Введите число: ");
+                        int numberT3 = Convert.ToInt32(Console.ReadLine());
+
+                        metods.Task3(numberT3);
+                        break;
+                    case 4:
+                        Console.WriteLine("\nЗадание 4: В банке в зависимости от суммы вклада начисляемый процент по вкладу может отличаться. " +
+                            "Напишите консольную программу, в которую пользователь вводит сумму вклада. Если сумма вклада меньше 100, " +
+                            "то начисляется 7%. Если сумма вклада от 100 до 200, то начисляется 9%. Если сумма вклада больше 200, то начисляется 12%. " +
+                            "В конце программа должна выводить сумму вклада с начисленными процентами.\n");
+
+                        Console.WriteLine("Внесите сумму вклада:");
+                        double numberT4 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Введите на сколько лет хотите взять в клад: ");
+                        double yearT4 = Convert.ToDouble(Console.ReadLine());
+
+                        metods.Task4(numberT4, yearT4);
+                        break;
+                    case 5:
+                        Console.WriteLine("\nЗадание 5: Изменим предыдущую задачу. Допустим, банк периодически начисляет по всем вкладам кроме процентов бонусы. " +
+                            "И, допустим, сейчас банк решил доначислить по всем вкладам 15 единиц вне зависимости от их суммы. " +
+                            "Измените программу таким образом, чтобы к финальной сумме дочислялись бонусы.\n");
+
+                        Console.WriteLine("Внесите сумму вклада:");
+                        double numberT5 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите на сколько лет хотите взять в клад: ");
+                        double yearT5 = Convert.ToDouble(Console.ReadLine());
+                        double bonusT5 = 15;
+
+                        metods.Task5(numberT5, yearT5, bonusT5);
+                        break;
+
+
+                }
+
+                Console.WriteLine("\nЧто бы закончить, нажмите клавишу 'q', если хотите продолжить нажмите любую клавишу. ");
+                cki = Console.ReadKey(true);
+
+            } while(cki.Key != ConsoleKey.Q);
+
+        }
+    }
+}
